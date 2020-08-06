e7a92420e9ee34a7f2a5251039a93ece8123a449
orb1805 <orb1805@yandex.ru>
2020-08-06 16:33:05 +0300

    First step

--- app/src/main/java/e/roman/greateapp/LaunchActivity.kt
+++ app/src/main/java/e/roman/greateapp/LaunchActivity.kt
@@ -1,5 +1,6 @@
 package e.roman.greateapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,16 @@ class LaunchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch)
+        //инциализация всех ресурсов
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val intent = Intent(this, LogRegActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/e/roman/greateapp/LogRegActivity.kt
@@ -0,0 +1,11 @@
+package e.roman.greateapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogRegActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_reg)
+    }
+}
