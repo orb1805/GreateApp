60ff984bd9a89f3864c4793a6f7db1943038b4b0
orb1805 <orb1805@yandex.ru>
2021-04-30 22:52:20 +0300

    Архитектурировал главную страницу

--- app/src/main/java/e/roman/greateapp/activities/MainScreenActivity.kt
+++ app/src/main/java/e/roman/greateapp/activities/MainScreenActivity.kt
@@ -4,22 +4,28 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.Toast
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
 import e.roman.greateapp.R
 import e.roman.greateapp.controllers.DataBase
 
-class MainScreenActivity : AppCompatActivity(), View.OnClickListener{
+class MainScreenActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var btnPerson: Button
     private lateinit var btnMyEvents: Button
     private lateinit var layout: LinearLayout
-    private lateinit var base: FirebaseFirestore
+
+    //private lateinit var base: FirebaseFirestore
     private lateinit var buttons: MutableList<Button>
     private lateinit var namesStr: String
     private lateinit var names: MutableList<String>
+    private lateinit var text: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,11 +34,11 @@ class MainScreenActivity : AppCompatActivity(), View.OnClickListener{
         btnPerson = findViewById(R.id.btn_person)
         btnMyEvents = findViewById(R.id.btn_my_events)
         layout = findViewById(R.id.layout)
-        base = DataBase.dataBase//FirebaseFirestore.getInstance()
+        //base = DataBase.dataBase//FirebaseFirestore.getInstance()
         buttons = mutableListOf()
         namesStr = intent.getStringExtra(getString(R.string.field_names))
         if (namesStr != "") {
-            names = namesStr.split( getString(R.string.delimiter)) as MutableList<String>
+            names = namesStr.split(getString(R.string.delimiter)) as MutableList<String>
             names.removeAt(0)
             this.updateList()
         }
@@ -43,9 +49,9 @@ class MainScreenActivity : AppCompatActivity(), View.OnClickListener{
         this.updateList()
     }
 
-    private fun updateList(){
+    private fun updateList() {
         if (buttons.isEmpty()) {
-            for (i in names.indices){
+            for (i in names.indices) {
                 buttons.add(Button(layout.context))
                 val params = LinearLayout.LayoutParams(
                     LinearLayout.LayoutParams.MATCH_PARENT,
@@ -57,11 +63,11 @@ class MainScreenActivity : AppCompatActivity(), View.OnClickListener{
                 layout.addView(buttons.last())
                 buttons.last().setOnClickListener(this)
             }
-        }
-        else{
-            val sharedPrefs = getSharedPreferences(getString(R.string.shared_prefs_name), Context.MODE_PRIVATE)
+        } else {
+            val sharedPrefs =
+                getSharedPreferences(getString(R.string.shared_prefs_name), Context.MODE_PRIVATE)
             val newName = sharedPrefs.getString("new name", "--")
-            if (newName != "--"){
+            if (newName != "--") {
                 buttons.add(Button(layout.context))
                 val params = LinearLayout.LayoutParams(
                     LinearLayout.LayoutParams.MATCH_PARENT,
@@ -77,8 +83,16 @@ class MainScreenActivity : AppCompatActivity(), View.OnClickListener{
         }
     }
 
-    override fun onClick(view: View?){
-        val text = (view as Button).text.toString()
+    override fun onClick(view: View?) {
+        text = (view as Button).text.toString()
+        DataBase.getDocumentWithEqual(
+            getString(R.string.coll_path_events),
+            getString(R.string.field_name),
+            text,
+            ::startEventView,
+            ::onFailure
+        )
+        /*
         var owner = "--"
         val intent = Intent(this, EventActivity::class.java)
         val bundle = Bundle()
@@ -106,6 +120,60 @@ class MainScreenActivity : AppCompatActivity(), View.OnClickListener{
                 intent.putExtras(bundle)
                 startActivity(intent)
             }
+        }*/
+    }
+
+    private fun startEventView(documents: QuerySnapshot) {
+        var owner = "--"
+        val intent = Intent(this, EventActivity::class.java)
+        val bundle = Bundle()
+        for (doc in documents) {
+            owner = doc[getString(R.string.field_owner)].toString()
+            bundle.putString(getString(R.string.field_name), text)
+            bundle.putString(getString(R.string.field_owner), owner)
+            bundle.putString(
+                getString(R.string.coll_path_universities),
+                doc[getString(R.string.coll_path_universities)].toString()
+            )
+            val checks = doc[getString(R.string.field_checks)].toString()
+                .split(" ") //0-description 1-date 2-time 3-people 4-price 5-phone
+            bundle.putString(
+                getString(R.string.field_checks),
+                doc[getString(R.string.field_checks)].toString()
+            )
+            if (checks[0] == "1")
+                bundle.putString(
+                    getString(R.string.field_description),
+                    doc[getString(R.string.field_description)].toString()
+                )
+            if (checks[1] == "1")
+                bundle.putString(
+                    getString(R.string.field_date),
+                    doc[getString(R.string.field_date)].toString()
+                )
+            if (checks[2] == "1")
+                bundle.putString(
+                    getString(R.string.field_time),
+                    doc[getString(R.string.field_time)].toString()
+                )
+            if (checks[3] == "1")
+                bundle.putString(
+                    getString(R.string.field_people),
+                    doc[getString(R.string.field_people)].toString()
+                )
+            if (checks[4] == "1")
+                bundle.putString(
+                    getString(R.string.field_price),
+                    doc[getString(R.string.field_price)].toString()
+                )
+            if (checks[5] == "1")
+                bundle.putString(
+                    getString(R.string.field_phone),
+                    doc[getString(R.string.field_phone)].toString()
+                )
+            bundle.putString(getString(R.string.field_id), doc.id)
+            intent.putExtras(bundle)
+            startActivity(intent)
         }
     }
 
@@ -118,5 +186,9 @@ class MainScreenActivity : AppCompatActivity(), View.OnClickListener{
 
     override fun finish() {
         //чтобы нельзя было выйти из активити через встроенную кнопку "назад"
+    }
+
+    private fun onFailure() {
+        Toast.makeText(this, "Ошибка", Toast.LENGTH_SHORT).show()
     }
 }
